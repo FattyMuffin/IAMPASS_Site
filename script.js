20fbc463931b9dea18aef520b37cc37a91d3f69a
BooCat <38902334+FattyMuffin@users.noreply.github.com>
2025-02-23 16:39:48 -0800

    Create script.js
    Basic Interactivity and Animation

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const ctaButton = document.querySelector(".cta-btn");
+    
+    ctaButton.addEventListener("click", () => {
+        alert("Thank you for your interest! We'll be in touch soon.");
+    });
+});
